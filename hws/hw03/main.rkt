457476ca2115472a70e90b1bd059b35e4ee34dd8
iambrj <joshibharathiramana@gmail.com>
2020-09-04 03:14:21 +0530

    Update tangled main.rkt

--- hws/hw03/main.rkt
+++ hws/hw03/main.rkt
@@ -1,13 +1,15 @@
 #lang racket
-
 (require eopl)
+(require rackunit)
+(require racket/match)
+(provide (all-defined-out))
+
 
 (define-datatype ast ast?
-  (binop (type binop?) (left ast?) (right ast?))
-  (ifte (c ast?) (t ast?) (e ast?))
-  (num (n number?))
-  (bool (b boolean?)))
-
+ [binop (op binop?) (rand1 ast?) (rand2 ast?)]
+ [ifte (c ast?) (t ast?) (e ast?)]
+ [num (n number?)]
+ [bool (b boolean?)])
 (define binop?
   (lambda (x)
     (match x
@@ -18,38 +20,31 @@
       ['lt? #t]
       ['eq? #t]
       [_ #f])))
-
-(define op-interpretation
-  (lambda (op)
-    (match op
-      ['add +]
-      ['sub -]
-      ['mul *]
-      ['div /]
-      ['lt? <]
-      ['eq? =]
-      [_ error 'op-interpretation "unknown op"])))
-
 (struct exn:parse-error exn:fail ())
-(define raise-parse-error
-  (lambda (err)
-    (raise (exn:parse-error err (current-continuation-marks)))))
+(define raise-parse-error 
+ (lambda (err-msg)
+   (raise (exn:parse-error err-msg (current-continuation-marks)))))
+;;; parse :: any/c -> ast?  Raises exception exn:parse-error?
+;;; Fill in the function parse here
+(define (parse exp)
+  (cond
+  [(number? exp) (num exp)]
+  [(boolean? exp) (bool exp)]
+  [(is-binop? (first exp)) (binop (binop->ast (first exp)) (parse (second exp)) (parse (third exp)))]
+  [(eq? (first exp) 'if) (ifte (parse (second exp)) (parse (third exp)) (parse (fourth exp)))]
+  [else (raise-parse-error "Parser error")]
+  ))
 
 (struct exn:exec-div-by-zero exn:fail ())
 (define raise-exec-div-by-zero
   (lambda ()
     (raise (exn:exec-div-by-zero "div-by-0!" (current-continuation-marks)))))
 
-(struct exn:exec-type-error exn:fail ())
-(define raise-exec-type-error
-  (lambda ()
-    (raise (exn:exec-type-error "types errored!" (current-continuation-marks)))))
-
 (struct exn:exec-type-mismatch exn:fail ())
 (define raise-exec-type-mismatch
   (lambda ()
     (raise (exn:exec-type-mismatch "type mismatch!" (current-continuation-marks)))))
-
+;;; runtime-check :: [expressible? -> boolean?], exn? -> [expressible? -> expressible? || exn?] 
 (define runtime-check
   (lambda (pred? exn)
     (lambda (v)
@@ -60,34 +55,53 @@
 (define typecheck-num
   (runtime-check number?  raise-exec-type-mismatch))
 
-(define typecheck-bool
+(define typecheck-bool 
   (runtime-check boolean? raise-exec-type-mismatch))
 
 (define check-non-zero
   (runtime-check (not/c zero?) raise-exec-div-by-zero))
+(define op-interpretation
+  (lambda (op)
+    (match op
+      ['add +]
+      ['sub -]
+      ['mul *]
+      ['div /]
+      ['lt? <]
+      ['eq? =]
+      [_ error "unknown op"])))
+;;; eval-ast :: ast? -> expressible? || (or/c exn:exec-div-by-zero  exn:exec-type-mismatch)
+(define eval-ast
+  (lambda (a)
+  (cases ast a
+      (binop (op l r) (if (and (equal? op 'div) (equal? (eval-ast r) 0))
+			    (raise-exec-div-by-zero)
+			    ((op-interpretation op)
+			      (typecheck-num (eval-ast l))
+			      (typecheck-num (eval-ast r)))))
+      (ifte (c t e) (if (typecheck-bool (eval-ast c)) (eval-ast t) (eval-ast e)))
+      (num (n) n)
+      (bool (b) b))))
+(define ts-numop-incorrect-param-rand1
+  (test-suite 
+   "wrongly typed rand1 parameters"
+   (for/list ([numerical-op '(add sub mul div lt? eq?)])
+     (test-case (string-append (symbol->string numerical-op) "-type-mismatch-rand1")
+       (check-exn exn:exec-type-mismatch?
+                  (lambda () 
+                    (eval-ast (binop numerical-op
+                                     (binop 'lt? (num 10) (num 20)) ; boolean
+                                     (num 10)))))))))
 
-#|
-(define ts-parsing
-  (test-suite "parsing"
-              (test-case "num" (check-equal? (parse 10) (ast-num 10)))
-              (test-case "add" (check-equal? (parse '(+ 10 20)) (ast-binop 'add (ast-num 10) (ast-num 20))))
-              (test-case "sub" (check-equal? (parse '(- 10 20)) (ast-binop 'sub (ast-num 10) (ast-num 20))))
-              (test-case "mul" (check-equal? (parse '(* 10 20)) (ast-binop 'mul (ast-num 10) (ast-num 20))))
-              (test-case "div" (check-equal? (parse '(/ 10 20)) (ast-binop 'div (ast-num 10) (ast-num 20))))
-              (test-case "bool-t" (check-equal? (parse #t) (ast-bool #t)))
-              (test-case "bool-f" (check-equal? (parse #f) (ast-bool #f)))
-              (test-case "if" (check-equal? (parse '(if #t 10 20)) (ast-if (ast-bool #t) (ast-num 10) (ast-num 20))))
-              (test-case "failure"
-                (check-exn exn:parse-error?
-                           (lambda () (parse '(** 10 20)))))
-              (test-case "recur" (check-equal?
-                                  (parse '(+ (- 10 20) (* 20 30)))
-                                  (ast-binop 'add
-                                             (ast-binop 'sub (ast-num 10) (ast-num 20))
-                                             (ast-binop 'mul (ast-num 20) (ast-num 30)))))
-              ))
-|#
-
+(define ts-numop-incorrect-param-rand2
+  (test-suite
+   "wrongly typed rand2 parameters"
+   (for/list ([numerical-op '(add sub mul div)])
+     (test-case (string-append (symbol->string numerical-op) "-type-mismatch-rand1")
+       (check-exn exn:exec-type-mismatch?
+                  (lambda () 
+                    (eval-ast (binop numerical-op (num 10)
+                                     (binop 'lt? (num 10) (num 20))))))))))
 (define (is-binop? t)
   (match t
     ['+ #t]
@@ -109,19 +123,3 @@
     ['== 'eq]
     [_ 'error]
     ))
-
-(define (parse exp)
-      (cond
-      [(number? exp) (num exp)]
-      [(boolean? exp) (bool exp)]
-      [(is-binop? (first exp)) (binop (binop->ast (first exp)) (parse (second exp)) (parse (third exp)))]
-      [(eq? (first exp) 'if) (ifte (parse (second exp)) (parse (third exp)) (parse (fourth exp)))]
-      [else raise-parse-error]
-      ))
-
-(define (eval-ast a)
-  (cases ast a
-      (binop (op l r) ((op-interpretation op) (eval-ast l) (eval-ast r)))
-      (ifte (c t e) (if (eval-ast c) (eval-ast t) (eval-ast e)))
-      (num (n) n)
-      (bool (b) b)))
