7b638e181953a6cee0d90c86756eb8f35c0a7cf0
iambrj <joshibharathiramana@gmail.com>
2020-12-17 17:43:54 +0530

    Fix formatting issues

--- src/hw04/main.rkt
+++ src/hw04/main.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
 (require eopl)
+(require rackunit)
+(require racket/match)
+(provide (all-defined-out))
+
 
 (define-datatype ast ast?
   [unaryop (op unaryop?) (rand ast?)]
@@ -10,7 +14,6 @@
   [bool (b boolean?)]
   [id-ref (sym id?)]
   [assume (bindings (list-of bind?)) (body ast?)])
-
 (define-datatype bind bind?
   [make-bind (b-id id?) (b-ast ast?)])
 
@@ -40,6 +43,12 @@
     (or (number? thing)
       (boolean? thing))))
 
+(struct exn:parse-error exn:fail ())
+
+(define raise-parse-error
+ (lambda (err-msg)
+   (raise (exn:parse-error err-msg (current-continuation-marks)))))
+
 (define (op->ast b)
   (match b
     ['+ 'add]
@@ -48,8 +57,7 @@
     ['/ 'div]
     ['< 'ltj]
     ['== 'eq]
-    [_ 'error]
-    ))
+    [_ 'error]))
 
 (define (is-binop? b)
   (match b
@@ -59,40 +67,36 @@
     ['/ #t]
     ['< #t]
     ['== #t]
-    [_ #f]
-    ))
+    [_ #f]))
 
 (define (is-unaryop? b)
   (match b
     ['! #t]
-    [_ #f]
-    ))
+    [_ #f]))
 
 ;;; parse :: any/c -> ast?  Raises exception exn:parse-error?
-;;; Fill in the function parse here
 (define (parse exp)
-  ;; complete the definition
     (cond
       [(number? exp) (num exp)]
       [(boolean? exp) (bool exp)]
       [(symbol? exp) (id-ref exp)]
-      [(equal? (first exp) 'if) (ifte (parse (second exp)) (parse (third exp)) (parse (fourth exp)))]
-      [(equal? (first exp) 'assume) (assume (map (lambda (binding) (make-bind
-                                                                     (first
-                                                                       binding)
-                                                                     (parse
-                                                                       (second
-                                                                         binding)))) (second exp)) (parse (third exp)))]
-      [(is-unaryop? (first exp)) (unaryop 'neg (parse (second exp)))]
-      [(is-binop? (first exp)) (binop (op->ast (first exp)) (parse (second exp)) (parse (third exp)))]
-      [else (raise-parse-error "Parser error")]
-      ))
-
-(struct exn:parse-error exn:fail ())
-
-(define raise-parse-error
- (lambda (err-msg)
-   (raise (exn:parse-error err-msg (current-continuation-marks)))))
+      [(equal? (first exp) 'if) (if (< (length exp) 4)
+				    (raise-parse-error "Missing body")
+				    (ifte (parse (second exp)) (parse (third exp)) (parse (fourth exp))))]
+      [(equal? (first exp) 'assume) (if (< (length exp) 3)
+				    (raise-parse-error "Missing body")
+				    (assume (map
+					(lambda (binding)
+					    (make-bind (first binding) (parse (second binding))))
+					(second exp))
+				    (parse (third exp))))]
+      [(is-unaryop? (first exp)) (if (< (length exp) 2)
+				     (raise-parse-error "Missing body")
+				     (unaryop 'neg (parse (second exp))))]
+      [(is-binop? (first exp)) (if (< (length exp) 2)
+				      (raise-parse-error "Missing body")
+				      (binop (op->ast (first exp)) (parse (second exp)) (parse (third exp))))]
+      [else (raise-parse-error "Parser error")]))
 
 (define-datatype env env?
   [empty-env]
@@ -106,7 +110,7 @@
   (lambda (e)
     (cases env e
       [empty-env () #t]
-      [else (#f)])))
+      [else #f])))
 
 ;;; extended-env? : env? -> boolean?
 (define extended-env?
@@ -119,31 +123,32 @@
 (define lookup-env
   (lambda (e x)
     (cases env e
-           [empty-env () raise-lookup-error]
+           [empty-env () (raise-lookup-error)]
            [extended-env (syms vals outer)
                          (let ([idx (index-of syms x)])
                            (if idx
                                (list-ref vals idx)
-                               (lookup-env outer x)))]))) ;; your solution here.
-
-(define mock-env (extended-env (list 'x) (list 10) (empty-env)))
+                               (lookup-env outer x)))])))
 
 (struct exn:lookup-error exn:fail ())
-(define raise-lookup-error 
+
+(define raise-lookup-error
   (lambda ()
     (raise (exn:lookup-error "unbound identifier" (current-continuation-marks)))))
 
 (struct exn:exec-div-by-zero exn:fail ())
+
 (define raise-exec-div-by-zero
   (lambda ()
     (raise (exn:exec-div-by-zero "div-by-0!" (current-continuation-marks)))))
 
 (struct exn:exec-type-mismatch exn:fail ())
+
 (define raise-exec-type-mismatch
   (lambda ()
     (raise (exn:exec-type-mismatch "type mismatch!" (current-continuation-marks)))))
 
-;;; runtime-check :: [expressible? -> boolean?], exn? -> [expressible? -> expressible? || exn?] 
+;;; runtime-check :: [expressible? -> boolean?], exn? -> [expressible? -> expressible? || exn?]
 (define runtime-check
   (lambda (pred? exn)
     (lambda (v)
@@ -154,7 +159,7 @@
 (define typecheck-num
   (runtime-check number?  raise-exec-type-mismatch))
 
-(define typecheck-bool 
+(define typecheck-bool
   (runtime-check boolean? raise-exec-type-mismatch))
 
 (define check-non-zero
@@ -172,32 +177,26 @@
       ['neg not]
       [_ error 'op-interpretation "unknown op"])))
 
-
-;;; eval-ast :: [ast? env?] -> expressible-value? 
-;;;                         || (or/c exn:exec-div-by-zero  exn:exec-type-mismatch exn:lookup-error)
 (define eval-ast
   (lambda (a e)
-    ;; your solution here
     (cases ast a
            [unaryop (op arg) (not (typecheck-bool (eval-ast arg e)))]
            [binop (op arg1 arg2) ((op-interpretation op)
                                   (typecheck-num (eval-ast arg1 e))
                                   (if (equal? op 'div)
-                                      (check-non-zero (typecheck-num
-                                                        ((eval-ast arg2 e))))
-                                      (typecheck-num ((eval-ast arg2 e)))))]
+                                      (check-non-zero (typecheck-num (eval-ast arg2 e)))
+                                      (typecheck-num (eval-ast arg2 e))))]
            [ifte (pred then otherwise) (if (typecheck-bool (eval-ast pred e))
                                            (eval-ast then e)
                                            (eval-ast otherwise e))]
-           [num (n) (n)]
-           [bool (b) (b)]
+           [num (n) n]
+           [bool (b) b]
            [assume (binds expr) (eval-ast
                                   expr
                                   (extended-env
                                     (map (lambda (x) (bind-id x)) binds)
-                                    (map (lambda (x) (bind-ast x)) binds) e))]
-           [id-ref (id) (lookup-env e id)]
-           )))
+                                    (map (lambda (x) (eval-ast (bind-ast x) e)) binds) e))]
+           [id-ref (id) (lookup-env e id)])))
 
 (define unaryop?
   (lambda (x)
@@ -215,3 +214,26 @@
       ['lt? #t]
       ['eq? #t]
       [_ #f])))
+
+(define ts-numop-incorrect-param-rand1
+  (test-suite
+   "wrongly typed rand1 parameters"
+   (for/list ([numerical-op '(add sub mul div lt? eq?)])
+     (test-case (string-append (symbol->string numerical-op) "-type-mismatch-rand1")
+       (check-exn exn:exec-type-mismatch?
+                  (lambda ()
+                    (eval-ast (binop numerical-op
+                                     (binop 'lt? (num 10) (num 20)) ; boolean
+                                     (num 10))
+                              (empty-env))))))))
+
+(define ts-numop-incorrect-param-rand2
+  (test-suite
+   "wrongly typed rand2 parameters"
+   (for/list ([numerical-op '(add sub mul div)])
+     (test-case (string-append (symbol->string numerical-op) "-type-mismatch-rand1")
+       (check-exn exn:exec-type-mismatch?
+                  (lambda ()
+                    (eval-ast (binop numerical-op (num 10)
+                                     (binop 'lt? (num 10) (num 20)))
+                              (empty-env))))))))
