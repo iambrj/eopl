449d159563fc979f6b1d65ba03bde3f8f00c0f81
iambrj <joshibharathiramana@gmail.com>
2020-12-17 17:41:11 +0530

    Fix file formatting

--- src/hw03/main.rkt
+++ src/hw03/main.rkt
@@ -4,12 +4,12 @@
 (require racket/match)
 (provide (all-defined-out))
 
-
 (define-datatype ast ast?
  [binop (op binop?) (rand1 ast?) (rand2 ast?)]
  [ifte (c ast?) (t ast?) (e ast?)]
  [num (n number?)]
  [bool (b boolean?)])
+
 (define binop?
   (lambda (x)
     (match x
@@ -20,10 +20,13 @@
       ['lt? #t]
       ['eq? #t]
       [_ #f])))
+
 (struct exn:parse-error exn:fail ())
+
 (define raise-parse-error 
  (lambda (err-msg)
    (raise (exn:parse-error err-msg (current-continuation-marks)))))
+
 ;;; parse :: any/c -> ast?  Raises exception exn:parse-error?
 ;;; Fill in the function parse here
 (define (parse exp)
@@ -32,8 +35,7 @@
   [(boolean? exp) (bool exp)]
   [(is-binop? (first exp)) (binop (binop->ast (first exp)) (parse (second exp)) (parse (third exp)))]
   [(eq? (first exp) 'if) (ifte (parse (second exp)) (parse (third exp)) (parse (fourth exp)))]
-  [else (raise-parse-error "Parser error")]
-  ))
+  [else (raise-parse-error "Parser error")]))
 
 (struct exn:exec-div-by-zero exn:fail ())
 (define raise-exec-div-by-zero
@@ -41,9 +43,11 @@
     (raise (exn:exec-div-by-zero "div-by-0!" (current-continuation-marks)))))
 
 (struct exn:exec-type-mismatch exn:fail ())
+
 (define raise-exec-type-mismatch
   (lambda ()
     (raise (exn:exec-type-mismatch "type mismatch!" (current-continuation-marks)))))
+
 ;;; runtime-check :: [expressible? -> boolean?], exn? -> [expressible? -> expressible? || exn?] 
 (define runtime-check
   (lambda (pred? exn)
@@ -60,6 +64,7 @@
 
 (define check-non-zero
   (runtime-check (not/c zero?) raise-exec-div-by-zero))
+
 (define op-interpretation
   (lambda (op)
     (match op
@@ -70,6 +75,7 @@
       ['lt? <]
       ['eq? =]
       [_ error "unknown op"])))
+
 ;;; eval-ast :: ast? -> expressible? || (or/c exn:exec-div-by-zero  exn:exec-type-mismatch)
 (define eval-ast
   (lambda (a)
@@ -82,6 +88,7 @@
       (ifte (c t e) (if (typecheck-bool (eval-ast c)) (eval-ast t) (eval-ast e)))
       (num (n) n)
       (bool (b) b))))
+
 (define ts-numop-incorrect-param-rand1
   (test-suite 
    "wrongly typed rand1 parameters"
@@ -102,6 +109,7 @@
                   (lambda () 
                     (eval-ast (binop numerical-op (num 10)
                                      (binop 'lt? (num 10) (num 20))))))))))
+
 (define (is-binop? t)
   (match t
     ['+ #t]
@@ -110,8 +118,7 @@
     ['/ #t]
     ['< #t]
     ['== #t]
-    [_ #f]
-    ))
+    [_ #f]))
 
 (define (binop->ast b)
   (match b
@@ -121,5 +128,4 @@
     ['/ 'div]
     ['< 'ltj]
     ['== 'eq]
-    [_ 'error]
-    ))
+    [_ 'error]))
