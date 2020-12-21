0cd3154a4d9a2045ea7a510ac89437b926b53ea0
iambrj <joshibharathiramana@gmail.com>
2020-12-21 19:56:52 +0530

    cps-letrec: implement numbers, quote, symbols, variadic lambdas, application

---
+++ src/cps-letrec/cps-letrec.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(define (ext-env u v env)
+  (lambda (x)
+    (if (eq? x u)
+      v
+      (env x))))
+
+(define empty-env
+  (lambda (x)
+    (error x ": missing binding")))
+
+(define init-env
+  (foldr ext-env
+         empty-env
+         '(+ - * /)
+         `(,(lambda (u v) (+ u v))
+            ,(lambda (u v) (- u v))
+            ,(lambda (u v) (* u v))
+            ,(lambda (u v) (/ u v)))))
+
+(define (cps-letrec expr [env init-env])
+  (match expr
+    [(? number? expr) expr]
+    [`(quote ,expr) expr]
+    [(? symbol? expr) (env expr)]
+    [`(lambda ,(list args ...) ,body)
+      (lambda host-args
+        (cps-letrec body
+                    (foldr ext-env
+                           env
+                           args
+                           host-args)))]
+    [(list rator rands ...)
+     (apply (cps-letrec rator env)
+            (map (lambda (rand) (cps-letrec rand env)) rands))]))
