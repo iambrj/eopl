cf5493ea37b28a4f4f5a7cfc3f7a0f2ddac8db2f
iambrj <joshibharathiramana@gmail.com>
2021-01-02 11:58:22 +0530

    cps-letrec: fix bad tests

--- src/cps-letrec/tests.rkt
+++ src/cps-letrec/tests.rkt
@@ -9,58 +9,68 @@
     "Values"
     (test-case
       "Numbers"
-      (check-equal?  (cps-letrec '5) 5))
+      (check-equal? (cps-letrec '5 init-env identity) 5))
     (test-case
       "Symbols"
-      (check-equal?  (cps-letrec '(quote x)) (quote x)))))
+      (check-equal? (cps-letrec '(quote x) init-env identity) (quote x)))))
 
 (define expression-tests
   (test-suite
     "Lookup (+,-,*,/ should be in the environment)"
     (test-case
       "+"
-      (check-equal? (cps-letrec '(+ 2 3)) 5))
+      (check-equal? (cps-letrec '(+ 2 3) init-env identity) 5))
     (test-case
       "-"
-      (check-equal? (cps-letrec '(- 5 2)) 3))
+      (check-equal? (cps-letrec '(- 5 2) init-env identity) 3))
     (test-case
       "*"
-      (check-equal? (cps-letrec '(* 3 4)) 12))
+      (check-equal? (cps-letrec '(* 3 4) init-env identity) 12))
     (test-case
       "/"
-      (check-equal? (cps-letrec '(/ 14 7)) 2))))
+      (check-equal? (cps-letrec '(/ 14 7) init-env identity) 2))))
 
 (define lambda-tests
   (test-suite
     "Application tests"
     (test-case
       "Identity application"
-      (check-equal? (cps-letrec '((lambda (x) x) 5)) 5))
+      (check-equal? (cps-letrec '(identity 5) init-env identity) 5))
     (test-case
       "Square application"
-      (check-equal? (cps-letrec '((lambda (x) (* x x)) 5)) 25))
+      (check-equal? (cps-letrec '((lambda (x) (* x x)) 5)
+                                init-env
+                                identity) 25))
     (test-case
       "Thunk evaluation"
-      (check-equal? (cps-letrec '((lambda () (* 5 (/ 10 2))))) 25))))
+      (check-equal? (cps-letrec '((lambda () (* 5 (/ 10 2)))) init-env identity) 25))))
 
 (define let-tests
   (test-suite
     "let binding tests"
     (test-case
       "let binding a number"
-      (check-equal? (cps-letrec '(let ([x 5]) x)) 5))
+      (check-equal? (cps-letrec '(let ([x 5]) x) init-env identity) 5))
     (test-case
       "let binding a symbol"
-      (check-equal? (cps-letrec '(let ([x (quote x)]) x)) (quote x)))
+      (check-equal? (cps-letrec '(let ([x (quote x)]) x) init-env identity) (quote x)))
     (test-case
       "let binding infiltrating a lambda abstraction"
-      (check-equal? (cps-letrec '(let ([x (quote x)]) ((lambda (y) x) 5))) (quote x)))
+      (check-equal? (cps-letrec '(let ([x (quote x)]) ((lambda (y) x) 5))
+                                init-env
+                                identity)
+                    (quote x)))
     (test-case
       "let binding in a thunk"
-      (check-equal? (cps-letrec '(let ([x 3]) ((lambda () x)))) 3))
+      (check-equal? (cps-letrec '(let ([x 3]) ((lambda () x)))
+                                init-env
+                                identity)
+                    3))
     (test-case
       "let binding a lambda expression"
-      (check-equal? (cps-letrec '(let ([identity (lambda (x) x)]) (identity 5)))
+      (check-equal? (cps-letrec '(let ([identity (lambda (x) x)]) (identity 5))
+                                init-env
+                                identity)
                     5))))
 
 (define letrec-tests
@@ -72,31 +82,33 @@
                                                     (if (zero? x)
                                                       0
                                                       (+ 2 (double (- x 1)))))])
-                                   (double 3)))
+                                   (double 3)) init-env identity)
                     6))
     (test-case
       "letrec mutual recursion 1"
       (check-equal? (cps-letrec '(letrec ([even? (lambda (x) (if (zero? x) #t (odd? (- x 1))))]
                                           [odd? (lambda (x) (if (zero? x) #f (even? (- x 1))))])
-                                   (even? 4)))
+                                   (even? 4)) init-env identity)
                     #t))
     (test-case
       "letrec mutual recursion 2"
       (check-equal? (cps-letrec '(letrec ([even? (lambda (x) (if (zero? x) #t (not (- x 1))))]
                                           [odd? (lambda (x) (if (zero? x) #f (not (- x 1))))])
-                                   (odd? 4)))
+                                   (odd? 4)) init-env identity)
                     #f))
     (test-case
       "letrec mutual recursion 3"
       (check-equal? (cps-letrec '(letrec ([even? (lambda (x) (if (zero? x) #t (odd? (- x 1))))]
                                           [odd? (lambda (x) (if (zero? x) #f (even? (- x 1))))])
-                                   (even? 5)))
+                                   (even? 5)) init-env identity)
                     #f))
     (test-case
       "letrec mutual recursion 4"
       (check-equal? (cps-letrec '(letrec ([even? (lambda (x) (if (zero? x) #t (odd? (- x 1))))]
                                           [odd? (lambda (x) (if (zero? x) #f (even? (- x 1))))])
-                                   (odd? 5)))
+                                   (odd? 5))
+                                init-env
+                                identity)
                     #t))))
 
 (run-tests value-tests 'verbose)
